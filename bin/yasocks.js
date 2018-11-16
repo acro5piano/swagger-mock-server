f05026ddf5ad6bbe4d890a7b4f6f485b29051b95
gosho-kazuya <ketsume0211@gmail.com>
2018-11-16 20:26:11 +0800

    add error handling

--- bin/yasocks.js
+++ bin/yasocks.js
@@ -2,4 +2,13 @@
 
 'use strict'
 
+if (!process.argv[2]) {
+  console.log('Fatal: no swagger file given')
+  console.log()
+  console.log('Usage:')
+  console.log('       yasocks /path/to/swagger.json')
+  console.log('       yasocks http://example.com/swagger.json')
+  process.exit(1)
+}
+
 require('../')()
